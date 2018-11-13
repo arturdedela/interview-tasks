041512ccb4785c3d7a1037ea7c53dfc21b048fbb
Artur Dedela <arturdedela@gmail.com>
2018-11-13 03:50:15 +0300

    Added isPalindromeRecursion

--- isPalindrome.js
+++ isPalindrome.js
@@ -6,4 +6,23 @@ function isPalindrome(s) {
     return cleanStr === cleanStr.split('').reverse().join('');
 }
 
+function isPalindromeRecursion(s) {
+    if (s.length === 1) {
+        return true;
+    }
+
+    if (s[0] === s[s.length - 1]) {
+        if (s.length === 2) {
+            return true;
+        }
+
+        return isPalindromeRecursion(s.substring(1, s.length - 1));
+    } else {
+        return false;
+    }
+}
+
+
+console.log(isPalindromeRecursion('123321'));
+
 module.exports = isPalindrome;
