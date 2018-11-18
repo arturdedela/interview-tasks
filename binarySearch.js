8dacd66f7a018ad727814e038aff42be87d5eb0b
Artur Dedela <arturdedela@gmail.com>
2018-11-18 23:05:48 +0300

    Added findInsertIndex function for binary search

--- binarySearch.js
+++ binarySearch.js
@@ -20,3 +20,26 @@ function findIndex(arr, val) {
 
     return mid;
 }
+
+
+//Binary search
+function findInsertIndex(arr, val) {
+
+    const calcMid = (l, r) => Math.floor((l + r) / 2);
+    let left = 0, right = arr.length;
+    let mid = calcMid(left, right);
+
+    while (val !== arr[mid]) {
+        if (val > arr[mid]) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+        const prevMid = mid;
+        mid = calcMid(left, right);
+
+        if (prevMid === mid) return right;
+    }
+
+    return mid;
+}
