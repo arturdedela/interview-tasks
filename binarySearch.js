c73c11b511b57b93f7a202eed76b0e978ff4096c
Artur Dedela <arturdedela@gmail.com>
2018-11-18 22:57:11 +0300

    Added binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,22 @@
+
+// Binary search
+function findIndex(arr, val) {
+
+    const calcMid = (l, r) => Math.floor((l + r) / 2);
+    let left = 0, right = arr.length;
+    let mid = calcMid(left, right);
+
+    while (val !== arr[mid]) {
+        if (val > arr[mid]) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+        const prevMid = mid;
+        mid = calcMid(left, right);
+
+        if (prevMid === mid) return -1;
+    }
+
+    return mid;
+}
