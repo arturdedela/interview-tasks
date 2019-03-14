a5783a27d2a4acea6fa4e098b5271b523a7d59f0
Artur Dedela <arturdedela@gmail.com>
2019-03-14 23:31:46 +0300

    Add sign

--- EPI/Arrays/MultiplyIntegers.js
+++ EPI/Arrays/MultiplyIntegers.js
@@ -8,6 +8,10 @@
 function multiply(arr1, arr2) {
     let result = [];
     let overage = 0;
+    const sign = arr1[0] < 0 ^ arr2[0] < 0 ? -1 : 1;
+    arr1[0] = Math.abs(arr1[0]);
+    arr2[0] = Math.abs(arr2[0]);
+
     for (let i = arr1.length - 1; i >= 0; i--) {
         for (let j = arr2.length - 1; j >= 0; j--) {
             const mult = arr1[i] * arr2[j] + overage;
@@ -25,6 +29,8 @@ function multiply(arr1, arr2) {
     if (overage) {
         result.unshift(overage);
     }
+
+    result[0] *= sign;
 
     return result;
 }
